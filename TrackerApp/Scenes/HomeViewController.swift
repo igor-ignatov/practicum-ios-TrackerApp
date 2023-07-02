fd8a85ef25d7fd504ba0f2555ac6af453fa3d6a8
Igor Ignatov <kantorawb@gmail.com>
2023-07-02 21:42:55 +0200

    feat: save onboarding showed state

--- TrackerApp/Scenes/HomeViewController.swift
+++ TrackerApp/Scenes/HomeViewController.swift
@@ -2,23 +2,23 @@ import UIKit
 
 final class HomeViewController: UITabBarController {
     private var onboarding: UIViewController?
-
+    
     init(onboarding: UIViewController?) {
         self.onboarding = onboarding
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupTabs()
         setupTabBarAppearance()
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         presentOnboarding()
@@ -31,17 +31,17 @@ private extension HomeViewController {
     private func setupTabs() {
         let trackerVC = UINavigationController(rootViewController: TrackersViewController())
         let statisticsVC = UINavigationController(rootViewController: StatisticsViewController())
-
+        
         let controllers = [trackerVC, statisticsVC]
         controllers.forEach(prepare)
-
+        
         viewControllers = controllers
-
+        
         if let listItem = tabBar.items?.first {
             listItem.image = .asset(.trackerTabIcon)
             listItem.title = "Трекеры"
         }
-
+        
         if let profileItem = tabBar.items?.last {
             profileItem.image = .asset(.statisticsTabIcon)
             profileItem.title = "Статистика"
@@ -54,31 +54,31 @@ private extension HomeViewController {
 private extension HomeViewController {
     func prepare(_ navigationController: UINavigationController) {
         navigationController.navigationBar.prefersLargeTitles = true
-
+        
         navigationController.navigationBar.standardAppearance.largeTitleTextAttributes = [
             NSAttributedString.Key.foregroundColor: UIColor.asset(.black),
             NSAttributedString.Key.font: UIFont.asset(.ysDisplayBold, size: 34)
         ]
     }
-
+    
     func setupTabBarAppearance() {
         view.backgroundColor = .asset(.white)
-
+        
         let tabItemsAppearance = UITabBarItemAppearance()
         tabItemsAppearance.normal.titleTextAttributes = [
             NSAttributedString.Key.font: UIFont.asset(.ysDisplayMedium, size: 10)
         ]
-
+        
         let appearance = UITabBarAppearance()
         appearance.backgroundColor = .asset(.white)
         appearance.stackedLayoutAppearance = tabItemsAppearance
-
+        
         tabBar.standardAppearance = appearance
-
+        
         if #available(iOS 15.0, *) {
             tabBar.scrollEdgeAppearance = appearance
         }
-
+        
         tabBar.tintColor = .asset(.blue)
     }
 }
@@ -88,6 +88,14 @@ private extension HomeViewController {
 private extension HomeViewController {
     func presentOnboarding() {
         if let onboarding {
+            let userDefaults = UserDefaults.standard
+            
+            if let onboardingShowed = userDefaults.string(forKey: "onboardingShowed") {
+                return
+            } else {
+                userDefaults.set("true", forKey: "onboardingShowed")
+            }
+            
             onboarding.modalPresentationStyle = .overFullScreen
             present(onboarding, animated: false)
             self.onboarding = nil
